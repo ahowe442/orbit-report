f996e4961889af9d5b745a211178a6ffe9ba019a
Abby Personal Acct Howe <howe.abigail@gmail.com>
2019-12-19 21:24:25 -0600

    added sort functionality to the name and type columns of the tabe, added input text box and a tiny button.  check css for button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,10 @@ import { Satellite } from "./satellite";
 export class AppComponent {
   title = "oribit-report";
   sourceList: Satellite[];
+  displayList: Satellite[];
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl =
       "https://handlers.education.launchcode.org/static/satellites.json";
 
@@ -35,10 +37,24 @@ export class AppComponent {
               );
               this.sourceList.push(satellite);
             }
+            this.displayList = this.sourceList.slice(0);
             //console.log(this.sourceList);
           }.bind(this)
         );
       }.bind(this)
     );
+  }
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for (let i = 0; i < this.sourceList.length; i++) {
+      let name = this.sourceList[i].name.toLowerCase();
+      if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+      }
+    }
+    // assign this.displayList to be the the array of matching satellites
+    // this will cause Angular to re-make the table, but now only containing matches
+    this.displayList = matchingSatellites;
   }
 }
